84584bcafe3fbdeb8f4f806e0bb315dab703732e
James Winter <dissolutionapex@gmail.com>
2016-03-21 13:20:44 -0500

    Added result handler to record results to the configured data store.

---
+++ src/StackWarden.Monitoring/ResultHandling/RecordingResultHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using log4net;
+using StackWarden.Core.Extensions;
+using StackWarden.Core.Persistence;
+
+namespace StackWarden.Monitoring.ResultHandling
+{
+    public class RecordingResultHandler : IMonitorResultHandler
+    {
+        private readonly ILog _log;
+        private readonly IRepository _repository;
+
+        public RecordingResultHandler(ILog log, IRepository repository)
+        {
+            _log = log.ThrowIfNull(nameof(log));
+            _repository = repository.ThrowIfNull(nameof(repository));
+        }
+
+        public void Handle(MonitorResult result)
+        {
+            try
+            {
+                _repository.Save(result);
+            }
+            catch (Exception ex)
+            {
+                _log.Error("Failed to record result.", ex);
+            }
+        }
+    }
+}
---
+++ src/StackWarden.Monitoring/ResultHandling/RecordingResultHandlerFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using log4net;
+using StackWarden.Core.Configuration;
+using StackWarden.Core.Extensions;
+using StackWarden.Core.Persistence;
+using StackWarden.Monitoring.Configuration;
+
+namespace StackWarden.Monitoring.ResultHandling
+{
+    public class RecordingResultHandlerFactory : ResultHandlerConfigurationDrivenFactory<RecordingResultHandlerFactory.Configuration, RecordingResultHandler>
+    {
+        private readonly IRepository _repository;
+
+        public class Configuration : IConfiguration
+        {
+            public string Type { get; set; }
+        }
+
+        public RecordingResultHandlerFactory(string configPath, IConfigurationReader configurationReader, IRepository repository)
+            : base(configPath, configurationReader)
+        {
+            _repository = repository.ThrowIfNull(nameof(repository));
+        }
+
+        public override IEnumerable<string> SupportedValues => new [] { "Record" };
+
+        protected override RecordingResultHandler BuildFromConfig(Configuration config)
+        {
+            var log = LogManager.GetLogger(typeof (RecordingResultHandler));
+            var handler = new RecordingResultHandler(log, _repository);
+
+            return handler;
+        }
+    }
+}
