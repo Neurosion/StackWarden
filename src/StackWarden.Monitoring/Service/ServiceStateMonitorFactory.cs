76bd97c02fff9d53190e102a3fb27439fe52f2e9
neurosion <dissolutionapex@gmail.com>
2016-03-23 19:34:07 -0500

    Updated Service.State monitor factory to allow multiple machine names, service names, and service name patterns to minimize the number of configuration files required.

--- src/StackWarden.Monitoring/Service/ServiceStateMonitorFactory.cs
+++ src/StackWarden.Monitoring/Service/ServiceStateMonitorFactory.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System.ServiceProcess;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
 using log4net;
+using StackWarden.Core.Extensions;
 using StackWarden.Core.Configuration;
 using StackWarden.Monitoring.Configuration;
 
@@ -9,8 +13,9 @@ namespace StackWarden.Monitoring.Service
     {
         public class Configuration : MonitorConfiguration
         {
-            public string Machine { get; set; }
-            public string Service { get; set; }
+            public IEnumerable<string> MachineNames { get; set; }
+            public IEnumerable<string> ServiceNames { get; set; }
+            public IEnumerable<string> ServiceNamePatterns { get; set; }
         }
 
         public override IEnumerable<string> SupportedValues => new[] { "Service.State" };
@@ -19,11 +24,30 @@ namespace StackWarden.Monitoring.Service
             :base(configPath, configurationReader, resultHandlerFactory)
         { }
         
-        protected override StateMonitor BuildFromConfig(Configuration config)
+        protected override IEnumerable<StateMonitor> BuildFromConfig(Configuration config)
         {
-            var instance = new StateMonitor(LogManager.GetLogger(typeof(StateMonitor)), config.Machine, config.Service);
+            config.MachineNames.ThrowIfNullOrEmpty(nameof(config.MachineNames));
 
-            return instance;
+            if (config.ServiceNamePatterns.IsNullOrEmpty())
+            {
+                foreach (var currentMachineName in config.MachineNames)
+                    foreach (var currentServiceName in config.ServiceNames)
+                        yield return new StateMonitor(LogManager.GetLogger(typeof(StateMonitor)), currentMachineName, currentServiceName);
+            }
+                
+            foreach (var currentMachineName in config.MachineNames)
+            {
+                var matchingServiceNames = config.ServiceNamePatterns
+                                                 .SelectMany(p => ServiceController.GetServices(currentMachineName)
+                                                                                   .Where(x => Regex.IsMatch(x.ServiceName, p))
+                                                                                   .Select(x => x.ServiceName));
+
+                matchingServiceNames.ThrowIfNullOrEmpty(nameof(matchingServiceNames),
+                                                       $"No services found on '{currentMachineName}' with name matching patterns '{string.Join(", ", config.ServiceNamePatterns)}'.");
+
+                foreach (var currentServiceName in matchingServiceNames)
+                    yield return new StateMonitor(LogManager.GetLogger(typeof(StateMonitor)), currentMachineName, currentServiceName);
+            }
         }
     }
 }
