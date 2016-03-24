a01d79adaa58c9740e3d7f3ed7c158121d3a99e8
neurosion <dissolutionapex@gmail.com>
2016-03-23 19:36:21 -0500

    Updated message queue queue size monitor factory to allow multiple machine names and a queue name pattern in order to minimize the number of configuration files required.

--- src/StackWarden.Monitoring/MessageQueue/QueueSizeMonitorFactory.cs
+++ src/StackWarden.Monitoring/MessageQueue/QueueSizeMonitorFactory.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using log4net;
 using StackWarden.Core.Configuration;
+using StackWarden.Core.Extensions;
 using StackWarden.Monitoring.Configuration;
 
 namespace StackWarden.Monitoring.MessageQueue
 {
+    using MessageQueue = System.Messaging.MessageQueue;
+
     public class QueueSizeMonitorFactory : MonitorConfigurationDrivenFactory<QueueSizeMonitorFactory.Configuration, QueueSizeMonitor>
     {
         public class Configuration : MonitorConfiguration
         {
             public string Path { get; set; }
+            public IEnumerable<string> MachineNames { get; set; }
+            public string NamePattern { get; set; }
             public int? WarningThreshold { get; set; }
             public int? ErrorThreshold { get; set; }
         }
@@ -20,17 +27,43 @@ namespace StackWarden.Monitoring.MessageQueue
             :base(configPath, configurationReader, resultHandlerFactory)
         { }
         
-        protected override QueueSizeMonitor BuildFromConfig(Configuration config)
+        protected override IEnumerable<QueueSizeMonitor> BuildFromConfig(Configuration config)
         {
-            var instance = new QueueSizeMonitor(LogManager.GetLogger(typeof(QueueSizeMonitor)), config.Path);
+            if (string.IsNullOrWhiteSpace(config.NamePattern))
+            {
+                var instance = new QueueSizeMonitor(LogManager.GetLogger(typeof(QueueSizeMonitor)), config.Path);
+                ConfigureInstance(config, instance);
 
+                yield return instance;
+            }
+
+            config.MachineNames.ThrowIfNullOrEmpty(nameof(config.MachineNames), 
+                                                   "At least one machine name must be provided when a name pattern is used.");
+
+            var queueFormatNames = config.MachineNames
+                                         .SelectMany(x => MessageQueue.GetPrivateQueuesByMachine(x).Concat(MessageQueue.GetPublicQueuesByMachine(x)))
+                                         .Where(x => Regex.IsMatch(x.QueueName, config.NamePattern))
+                                         .Select(x => x.FormatName);
+
+            queueFormatNames.ThrowIfNullOrEmpty(nameof(queueFormatNames),
+                                                $"No message queues found on '{string.Join(",", config.MachineNames)}' with name matching pattern '{config.NamePattern}'.");
+
+            foreach (var currentFormatName in config.MachineNames)
+            {
+                var instance = new QueueSizeMonitor(LogManager.GetLogger(typeof(QueueSizeMonitor)), currentFormatName);
+                ConfigureInstance(config, instance);
+
+                yield return instance;
+            }
+        }
+
+        private void ConfigureInstance(Configuration config, QueueSizeMonitor instance)
+        {
             if (config.ErrorThreshold.HasValue)
                 instance.ErrorThreshold = config.ErrorThreshold.Value;
 
             if (config.WarningThreshold.HasValue)
                 instance.WarningThreshold = config.WarningThreshold.Value;
-
-            return instance;
         }
     }
 }
