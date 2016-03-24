36cc4d84b12dc4ed881bd15855ee71daf54111c2
neurosion <dissolutionapex@gmail.com>
2016-03-23 22:46:56 -0500

    Added a lock to the console result handler in order to keep colors/result details together.

--- src/StackWarden.Monitoring/ResultHandling/ConsoleResultHandler.cs
+++ src/StackWarden.Monitoring/ResultHandling/ConsoleResultHandler.cs
@@ -5,27 +5,32 @@ namespace StackWarden.Monitoring.ResultHandling
 {
     public class ConsoleResultHandler : IMonitorResultHandler
     {
+        private readonly object _handleLock = new object();
+
         public void Handle(MonitorResult result)
         {
-            switch (result.TargetState)
+            lock (_handleLock)
             {
-                case SeverityState.Normal:
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    break;
-                case SeverityState.Warning:
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    break;
-                case SeverityState.Error:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                default:
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    break;
-            }
-            Console.WriteLine($"Monitor: {result.SourceType}, Target: {result.TargetName}, State: {result.TargetState}");
+                switch (result.TargetState)
+                {
+                    case SeverityState.Normal:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case SeverityState.Warning:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case SeverityState.Error:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                }
+                Console.WriteLine($"Monitor: {result.SourceType}, Target: {result.TargetName}, State: {result.TargetState}");
 
-            if (!string.IsNullOrWhiteSpace(result.FriendlyMessage))
-                Console.WriteLine($"\tDetails: {result.FriendlyMessage}");
+                if (!string.IsNullOrWhiteSpace(result.FriendlyMessage))
+                    Console.WriteLine($"\tDetails: {result.FriendlyMessage}");
+            }
         }
     }
 }
