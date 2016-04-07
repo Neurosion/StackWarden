a83380e2480153e0d68869d841cc747320d64c5a
neurosion <dissolutionapex@gmail.com>
2016-04-07 01:12:27 -0500

    Wired recording result handler factory to repository factory.

---
+++ src/StackWarden.Core/Persistence/RepositoryFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using StackWarden.Core.Configuration;
+
+namespace StackWarden.Core.Persistence
+{
+    public class RepositoryFactory : ConfigurationDrivenFactory<RepositoryFactory.Configuration, IRepository>
+    {
+        public class Configuration : IConfiguration
+        {
+            public string Type { get; set; }
+        }
+
+        public override IEnumerable<string> SupportedValues => new [] { "SQL", "Memory" };
+
+        protected override string ConfigExtension => "repositoryconfig";
+
+        protected RepositoryFactory(string configPath, IConfigurationReader configurationReader) 
+            :base(configPath, configurationReader)
+        { }
+
+        protected override IEnumerable<IRepository> BuildFromConfig(Configuration config)
+        {
+            throw new NotImplementedException();
+            // todo: turn this into a composite factory, like monitor factory
+        }
+    }
+}
--- src/StackWarden.Monitoring/ResultHandling/RecordingResultHandlerFactory.cs
+++ src/StackWarden.Monitoring/ResultHandling/RecordingResultHandlerFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using log4net;
 using StackWarden.Core.Configuration;
 using StackWarden.Core.Extensions;
@@ -9,24 +10,32 @@ namespace StackWarden.Monitoring.ResultHandling
 {
     public class RecordingResultHandlerFactory : ResultHandlerConfigurationDrivenFactory<RecordingResultHandlerFactory.Configuration, RecordingResultHandler>
     {
-        private readonly IRepository _repository;
+        private readonly RepositoryFactory _repositoryFactory;
 
         public class Configuration : IConfiguration
         {
             public string Type { get; set; }
+            public string[] Repositories { get; set; }
         }
 
-        public RecordingResultHandlerFactory(string configPath, IConfigurationReader configurationReader, IRepository repository)
+        public RecordingResultHandlerFactory(string configPath, IConfigurationReader configurationReader, RepositoryFactory repositoryFactory)
             : base(configPath, configurationReader)
         {
-            _repository = repository.ThrowIfNull(nameof(repository));
+            _repositoryFactory = repositoryFactory.ThrowIfNull(nameof(repositoryFactory));
         }
 
         public override IEnumerable<string> SupportedValues => new [] { "Record" };
 
         protected override IEnumerable<RecordingResultHandler> BuildFromConfig(Configuration config)
         {
-            yield return new RecordingResultHandler(LogManager.GetLogger(typeof(RecordingResultHandler)), _repository);
+            config.Repositories.ThrowIfNullOrEmpty(nameof(config.Repositories));
+
+            var repositories = config.Repositories.SelectMany(x => _repositoryFactory.Build(x));
+
+            repositories.ThrowIfNullOrEmpty(nameof(repositories), $"No repository instances could be resolved from repositories: {string.Join(",", config.Repositories)}");
+
+            foreach (var currentRepository in repositories)
+                yield return new RecordingResultHandler(LogManager.GetLogger(typeof(RecordingResultHandler)), currentRepository);
         }
     }
 }
